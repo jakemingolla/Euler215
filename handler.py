97d88388ab5886fdd042881149ad831e4092b87d
Jake mingolla <jakemingolla@gmail.com>
2016-05-30 15:24:28 -0400

    changed to numbers

--- handler.py
+++ handler.py
@@ -4,10 +4,10 @@ stack_memos = {}
 total_memos = {}
 
 _total = []
-_current = ''
+_current = 0
 _length = 0
 
-bricks = ['2', '3']
+bricks = [2, 3]
 brick_index = 0
 
 height = 10
@@ -17,21 +17,19 @@ width = 32
 
 def add(brick):
     global _current, _length
+    _current = _current * 10
     _current += brick
-    _length += int(brick)
+    _length += brick
 
 def delete():
     global _current, _length
-    try:
-        _length -= int(_current[-1])
-    except IndexError:
-        _length = 0
-    _current = _current[:-1]
+    _length -= (_current % 10)
+    _current //= 10
 
 def push():
     global _total, _current, _length
     _total.insert(0, _current)
-    _current = ''
+    _current = 0
     _length = 0
 
 def pop():
@@ -40,17 +38,10 @@ def pop():
         _current = _total.pop(0)
         _length = width
     except IndexError:
-        _current = ''
+        _current = 0
         _length = 0
 
 
-#def current(index):
-#    global _current
-#    if len(current) <= index:
-#        return int(_current[index])
-#    else:
-#        return 0
-#
 #def prev(index):
 #    global _prev
 #    return int(_prev[index])
@@ -73,7 +64,7 @@ def over():
 def unique():
     global brick_index, _total, _current
     #s = str(brick_index) +'|' +  ''.join(_total) + '|' + _current
-    s = str(brick_index) + '|' + str(_total) + '|' + _current
+    s = str(brick_index) + '|' + str(_total) + '|' + str(_current)
     h = hashlib.md5()
     h.update(s)
     s = h.digest()
@@ -90,36 +81,47 @@ def stackable():
         return True
 
     _prev = _total[0]
-    memo = _current + ' ' + _prev
+    memo = str(_current) + '|' + str(_prev)
 
     try:
         return stack_memos[memo]
     except KeyError:
-        breaks = {}
-        counter = 0
-        for x in list(_prev):
-            counter += int(x)
-            breaks[counter] = True
+        cur_breaks = _length
+        prev_breaks = width
+        cur = _current
+        #TODO FIX ME
+        prev = _prev
 
-        counter = 0
-        for x in list(_current):
-            counter += int(x)
-            if counter in breaks and counter != width:
-                stack_memos[memo] = False
-                return False
+        while cur and prev:
+            #print 'cur_breaks = ' + str(cur_breaks)
+            #print 'prev_breaks = ' + str(prev_breaks)
+            #print 'cur = ' + str(cur)
+            #print 'prev = ' + str(prev)
+            if (prev_breaks < prev_breaks):
+                cur_breaks -= (cur % 10)
+                cur //= 10
+                if cur_breaks != 0 and cur_breaks == prev_breaks:
+                    stack_memos[memo] = False
+                    return False
+            else:
+                prev_breaks -= (prev % 10)
+                prev //= 10
+                if prev_breaks != 0 and prev_breaks == cur_breaks:
+                    stack_memos[memo] = False
+                    return False
 
-    stack_memos[memo] = True
-    return True
+        stack_memos[memo] = True
+        return True
 
 def main():
     global bricks, brick_index, _length
-    current_brick = ''
+    current_brick = 0
     rows = 0
 
     solutions = []
     
-    while not (_current == '' and brick_index == len(bricks)):
-        #print "current         = '" + _current + "'"
+    while not (_current == 0 and brick_index == len(bricks)):
+        #print "current         = " + str(_current)
         #print "total           = " + str(_total)
         #print "brick index     = " + str(brick_index)
         #print "_length         = " + str(_length)
@@ -144,7 +146,7 @@ def main():
 
         current_brick = bricks[brick_index]
         add(current_brick)
-        #print 'adding ' + current_brick
+        #print 'adding ' + str(current_brick)
 
         if over():
             #print 'over, deleting'
@@ -178,7 +180,6 @@ def main():
 
         #print '-----------------------'
     print 'found ' + str(len(solutions)) + ' solutions'
-
 
 
 if __name__ == "__main__":
